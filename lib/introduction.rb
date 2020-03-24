32ac9f7e7cade0b7430f28eca56ac26db99c25f6
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-24 20:01:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction(name,language) 
+def introduction(name,language)
   puts "Hi, my name is #{name} and I am learning how to program in #{language}."
 end
